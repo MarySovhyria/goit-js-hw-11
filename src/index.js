b957e924e84162d9b6b9b5b0e487b8f79b810ef2
MarySovhyria <111424962+MarySovhyria@users.noreply.github.com>
2023-09-03 23:13:32 +0100

    second commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import Notiflix from 'notiflix';
 
-// const keyWord = document.querySelector('input[name="searchQuery"]');
 const searchForm = document.querySelector('#search-form');
 const imageList = document.querySelector('.gallery');
 const loadMore = document.querySelector('.load-more');
